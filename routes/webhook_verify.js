50f49da45fe4af5ed9140eb0ff2735b74271c52b
unknown <anislouriche@gmail.com>
2023-05-12 22:37:35 +0000

    upload code

--- routes/webhook_verify.js
+++ routes/webhook_verify.js
@@ -2,7 +2,7 @@ const processPostback = require('../processes/postback');
 const processMessage = require('../processes/messages');
 module.exports = function(app, chalk){
   app.get('/webhook', function(req, res) {
-    if (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN){
+    if (req.query['hub.verify_token'] === "2289353934624857"){
        console.log('webhook verified');
        res.status(200).send(req.query['hub.challenge']);
     } else {
