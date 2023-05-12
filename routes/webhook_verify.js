9763915192f44e0e1654a35ffe2630a48edc3db0
Steve Walson <75564431+stevewalson@users.noreply.github.com>
2023-05-12 23:19:15 +0000

    Update webhook_verify.js

--- routes/webhook_verify.js
+++ routes/webhook_verify.js
@@ -1,6 +1,6 @@
 module.exports = function(app, chalk){
   app.get('/webhook', function(req, res) {
-    if (req.query['hub.verify_token'] === "2289353934624857"){
+    if (req.query['hub.verify_token'] === "EAAGMHbZAkHOwBAJOZCrZAz70tlTOCYIgsCZAEdPOq62zUcegNWZAIRFtMkSMP89TlcnEE8oSPytZAfxNZBmP3GTbEcUi3KB2MFD12r8BNI0K90qf1Hq9k3jZCjAHbXwT2g10ZAZAo06qiFp3xkXPxoZAHyWxxSY2HrrGJHBF8nAYGuH2aLFfE1aVMA4Ef4388dZAWksDQgkZAzVHkCgZDZD"){
        console.log('webhook verified');
        res.status(200).send(req.query['hub.challenge']);
     } else {
